8aa78058ba10d9b6f52a614a8fbe9c78f1278e93
adicwu <youzaiwu@qq.com>
2024-04-11 22:14:24 +0800

    feat

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,55 @@
 <template>
   <div class="app">
-    <b class="text-5xl">{{ cur }}</b>
+    <b class="text-5xl">{{ FingerType[type] }} 指型</b>
     <p class="flex gap-4 mt-4 text-lg p-2 border-2 flex-wrap">
       <span v-for="(item, i) in cache" :key="i">{{ item }}</span>
     </p>
 
     <div class="mt-4 flex gap-4">
-      <button @click="cache.splice(0)">清空缓存</button>
       <button @click="createCur">换一个</button>
-      <button @click="startCreateCur">随机{{ timer ? "停止" : "开始" }}</button>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const list = [
-  [
-    "升3",
-    "升4",
-    null,
-    "升5",
-    null,
-    "升6",
-    null,
-    "升7",
-    "升升1",
-    null,
-    "升升2",
-    null,
-    "升升3",
-    "升升4",
-  ],
-  [
-    "7",
-    "升1",
-    null,
-    "升2",
-    null,
-    "升3",
-    "升4",
-    null,
-    "升6",
-    null,
-    "升7",
-    "升升1",
-  ],
-  [
-    "5",
-    null,
-    "6",
-    null,
-    "7",
-    "升1",
-    null,
-    "升2",
-    null,
-    "升3",
-    "升4",
-    null,
-    "升5",
-  ],
-  ["2", null, "3", "4", null, "5", null, "6", null, "7", "升1", null, "升2"],
-  ["降6", null, "降7", "1", null, "2", null, "3", "4", null, "5", null, "6"],
-  [
-    "降3",
-    "降4",
-    null,
-    "降5",
-    null,
-    "降6",
-    null,
-    "降7",
-    "1",
-    null,
-    "2",
-    null,
-    "3",
-    "4",
-  ],
-].reverse();
+import _ from "lodash";
 
+const All = [
+  "降3",
+  "降4",
+  "降5",
+  "降6",
+  "降7",
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  "升1",
+  "升2",
+  "升3",
+  "升4",
+  "升5",
+  "升6",
+  "升7",
+  "升升1",
+  "升升2",
+  "升升3",
+  "升升4",
+];
+const list = [
+  All.slice(0, 17),
+  All.slice(2, 18),
+  All.slice(3, 20),
+  All.slice(4, 21),
+  All.slice(6, 24),
+];
+const FingerType = ["mi", "sol", "la", "xi", "re"];
+
+const type = 0;
 const cur = ref("");
 const timer = ref("");
 const cache = ref([]);
@@ -89,26 +58,10 @@ const random = (max, min = 0) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 const createCur = () => {
-  const n = random(list.length);
-  const x = list[n];
-  let res = null;
-  while (!res) {
-    res = x[random(x.length)];
-  }
-  cur.value = `${n + 1}弦-${res}`;
-  cache.value.push(cur.value);
-};
-const startCreateCur = () => {
-  if (timer.value) {
-    clearInterval(timer.value);
-    timer.value = null;
-  } else {
-    timer.value = setInterval(() => {
-      createCur();
-    }, 6000);
-  }
+  cache.value = _.shuffle(list[type]);
 };
 
+createCur();
 document.addEventListener("keydown", (e) => {
   if (e.keyCode === 32) {
     createCur();
